b43e2603810e9d0cc6940935b8d1280ed41e3701
EC2 Default User <ec2-user@ip-172-31-6-157.us-west-1.compute.internal>
2018-03-12 06:59:26 +0000

    Added server project

--- multi-market/resources/views/welcome.blade.php
+++ multi-market/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 3
+                    Laravel
                 </div>
 
                 <div class="links">
