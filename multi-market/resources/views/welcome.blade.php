50a699ca4bf03302dd9022cf7a735b6452098da1
Hussain <e.alhassan.h@gmail.com>
2018-03-14 10:46:52 +0000

    Making sure local and prod work fine

--- multi-market/resources/views/welcome.blade.php
+++ multi-market/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel local
+                    Laravel 3
                 </div>
 
                 <div class="links">
